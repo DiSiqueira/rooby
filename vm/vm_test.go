14c450811e3cfe2dd90127f631122b200328aff6
Stan Lo <a22301613@yahoo.com.tw>
2017-04-28 00:25:44 +0800

    Add file source to every instruction set to avoid override issue.

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -395,7 +395,7 @@ func checkParserErrors(t *testing.T, p *parser.Parser) {
 
 func testExec(bytecodes string) Object {
 	v := New("./")
-	v.ExecBytecodes(bytecodes)
+	v.ExecBytecodes(bytecodes, "./")
 
 	return v.stack.top().Target
 }
